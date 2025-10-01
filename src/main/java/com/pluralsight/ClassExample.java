a67891c08d57be82a6073bfc3ec5a8d140a2fdde
Romell Walker <walkerromell@gmail.com>
2025-10-01 15:51:40 -0400

    Altima is Helltima

--- src/main/java/com/pluralsight/ClassExample.java
+++ src/main/java/com/pluralsight/ClassExample.java
@@ -2,11 +2,10 @@ package com.pluralsight;
 
 public class ClassExample {
     public static void main(String[] args) {
-        Car car1= new Car();
+        Car car1 = new Car();
         car1.setMake("Nissan");
-        car1.setModel("Altima");
-        System.out.println(car1.getMake()+" "+car1.getModel());
-
+        car1.setModel("Helltima");
+        System.out.println(car1.getMake() + " " + car1.getModel());
 
 
     }
